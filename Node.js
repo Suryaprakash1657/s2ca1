979603d817a2c5204a2be5a74a1a0c9d33cf607b
Suryaprakash1657 <suryaprk165@gmail.com>
2025-02-10 14:37:13 +0530

    Node.js

--- Node.js
+++ Node.js
@@ -8,15 +8,15 @@ app.get('/',(req,res)=>{
 app.post('/login',(req,res)=>{
     const{username,password,dateofbirth}=req.body
     if(!username){
-        return res.status(400).json(message:"Username cannot be empty")
+        return res.status(400).send("Username cannot be empty")
     }
     if(!email){
-        return res.status(400).json(message:"Email cannot be empty")
+        return res.status(400).send("Email cannot be empty")
     }
     if(password.length<8 || password.length>16){
-        return res.status(400).json(message:"Password length should be greater than 8 or less than or equal to 16")
+        return res.status(400).send("Password length should be greater than 8 or less than or equal to 16")
     }
-    return res.status(200).json(message:"Login succesfull")
+    return res.status(200).send("Login succesfull")
 })
 
 
